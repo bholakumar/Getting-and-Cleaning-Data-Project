ca643c05e8215158bc8e2c6ceaa2f5dbe969788c
bholakumar <bholakumar@yahoo.com>
2016-06-29 00:23:16 +1000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,16 @@
+############################################################################################################
+#  Getting and Cleaning Data - Course Project
+#  June 2016
+#
+#  The purpose of this script is to:
+#  1. Merge the training and the test sets to create one data set.
+#  2. Extract only the measurements on the mean and standard deviation for each measurement.
+#  3. Use descriptive activity names to name the activities in the data set
+#  4. Appropriately label the data set with descriptive variable names.
+#  5. From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+#
+############################################################################################################
+
 path_rf<- "./data"
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
